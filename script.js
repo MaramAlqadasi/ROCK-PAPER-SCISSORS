c78f56d189d4f5e435dfbb1ccd98d1f7dbaaf4a6
MaramAlqadasi <maramalqadasi0@gmail.com>
2022-01-29 16:04:34 +0300

    Adding Game Function

--- script.js
+++ script.js
@@ -6,26 +6,25 @@ return selections[random];
 }
 function playRound(playerSelection, computerSelection) {
     // your code here!
-    let playerScores =0;
-    let computerScores = 0;
+    
     if(playerSelection==computerSelection){
-        console.log("Computer's Scores : " + computerScores + "  Player's Scors : " + playerScores );
-        return("It's Tie " +playerSelection+" tie with "+computerSelection)
+        //console.log("Computer's Scores : " + computerScores + "  Player's Scors : " + playerScores );
+        return("It's a Tie " +playerSelection+" tie with "+computerSelection)
     }
     else if (
         (playerSelection === "Rock" && computerSelection==="Paper")||
         (playerSelection === "Paper" && computerSelection==="Scissors")|| 
         (playerSelection === "Scissors" && computerSelection==="Rock")){
-            computerScores +=1;
-            console.log("Computer's Scores : " + computerScores + "  Player's Scors : " + playerScores );
+           
+           // console.log("Computer's Scores : " + computerScores + "  Player's Scors : " + playerScores );
             return "Computer win " +computerSelection +" beats "+ playerSelection;
     }
     else if (
         (playerSelection === "Rock" && computerSelection==="Scissors")||
         (playerSelection === "Paper" && computerSelection==="Rock")||
         (playerSelection === "Scissors" && computerSelection==="Paper")){
-            playerScores +=1;
-            console.log("Computer's Scores : " + computerScores + "  Player's Scors : " + playerScores );
+           // playerScores +=1;
+            //console.log("Computer's Scores : " + computerScores + "  Player's Scors : " + playerScores );
             return "You win " +playerSelection +" beats "+ computerSelection;
         }
         
@@ -38,9 +37,37 @@ function playRound(playerSelection, computerSelection) {
       let otherLetters= playerSelection.slice(1);
       return (firstLetter.toUpperCase()+ otherLetters.toLowerCase());
   }
+  function Game(){
+      let playerScores=0;
+      let computerScores=0;
+      for(let i=0;computerScores<5 && playerScores<5;i++){
+        const playerSelection = readPlayerSelection();
+        const computerSelection = computerPlay();
+        let winner=(playRound(playerSelection, computerSelection));
+        if(winner.includes("It's a Tie")){
+            i--;
+        }
+        else if(winner.includes("You win")){
+            playerScores+=1;
+
+        }
+        else {
+            computerScores+=1;
+        }
+        console.log(winner+" ...your  Scores: "+playerScores+"   Computer Scors: "+computerScores);
+
+      }
+      if(playerScores==5){
+      console.log("You are the Winner"+"...   Your Scores: "+playerScores+"   Computer Scors: "+computerScores);
+      }
+      else{
+        console.log("Computer is  the Winner"+"...your Scores: "+playerScores+"   Computer Scors: "+computerScores);
+
+      }
+
+  }
 
   
-  const playerSelection = readPlayerSelection();
-  const computerSelection = computerPlay();
- // console.log(game(playRound()));
-  console.log(playRound(playerSelection, computerSelection));
+//   const playerSelection = readPlayerSelection();
+//   const computerSelection = computerPlay();
+//   console.log(playRound(playerSelection, computerSelection));
