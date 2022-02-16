2e776bbc895bf3262a80ef40e9e319b4f0e1479c
MaramAlqadasi <maramalqadasi0@gmail.com>
2022-02-16 15:05:22 +0300

    update

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let computerScores=0;
 function computerPlay(){
     let selections=["Rock","Paper","Scissors"];
     const random = Math.floor(Math.random() * selections.length);
+    console.log(selections[random]);
 return selections[random];
 
 }
